941fcfeeee41637ecaf424432a3bc3fc7bac232f
PsMorales <luispa.sosa@gmail.com>
2020-10-01 00:20:05 -0600

    consumo de servicio obtener

--- front-end-app/src/app/components/menu/especialidades/especialidades.component.ts
+++ front-end-app/src/app/components/menu/especialidades/especialidades.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { EspecialidadesService } from './../../../services/especialidades.service';
+import { DatosEspecialidades } from './../../../models/especialidades';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+
+declare var $:any;
 
 @Component({
   selector: 'app-especialidades',
@@ -6,10 +16,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./especialidades.component.css']
 })
 export class EspecialidadesComponent implements OnInit {
+  displayedColumns: string[] = ['TxtNombres', 'TxtDireccion', 'TxtEmail', 'FechaIngreso', 'opciones'];
+  dataSource: MatTableDataSource<DatosEspecialidades>;
 
-  constructor() { }
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(
+    public dialogo: MatDialog,
+    private spinner: NgxSpinnerService,
+    private router: Router,
+    private especialidadesService: EspecialidadesService
+  ) { }
 
   ngOnInit(): void {
+    this.cargarEspecialidades();
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  cargarEspecialidades(){
+    this.spinner.show();
+    this.especialidadesService.ServicioObtenerEspecialidades().subscribe(resultado =>{
+      if(resultado[0].EstadoToken !== '0'){
+        this.dataSource = new MatTableDataSource(resultado);
+        
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.spinner.hide();
+      }
+      else{
+        this.spinner.hide();
+        sessionStorage.setItem("DatosUsuario", "");
+        sessionStorage.setItem("SessionStarted", "0");
+        this.router.navigate(['/login']);
+        this.Mensaje("Token del usuario activo invalido", 4, 1, 1);
+      }
+    },
+    error =>{
+      this.spinner.hide();
+      this.Mensaje(error.statusText, 4, 1, 1);
+    });
+  }
+
+  Mensaje(mensaje: any, color: number, posY:number, posX: number){
+    const type = ['','info','success','warning','danger'];
+    const from = ['', 'top', 'bottom'];
+    const align = ['', 'left', 'center', 'right'];
+
+    $.notify({
+        icon: "",
+        message: mensaje
+    },{
+        type: type[color],
+        timer: 1000,
+        placement: {
+            from: from[posY],
+            align: align[posX]
+        }
+    });
+  }
 }
