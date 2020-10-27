02befa18dfcc4569092772a94979480374af2d9b
PsMorales <luispa.sosa@gmail.com>
2020-10-27 12:08:23 -0600

    obtener factores

--- front-end-app/src/app/services/escalas-de-calificacion.service.ts
+++ front-end-app/src/app/services/escalas-de-calificacion.service.ts
@@ -1,9 +1,28 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EscalasDeCalificacionService {
+  escalaDeCalificacion = {
+    IdEscalaDeCalificacion: 0,
+    TxtToken: ''
+  };
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  ServicioObtenerFactores(): Observable<any>{
+    this.escalaDeCalificacion.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ObtenerFactores`, this.escalaDeCalificacion);
+  }
+
+  getToken(){
+    return JSON.parse(sessionStorage.getItem("DatosUsuario"));
+  }
 }
