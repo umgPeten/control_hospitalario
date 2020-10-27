0160a12bd0a2b79142d49199517ac77de5b10211
PsMorales <luispa.sosa@gmail.com>
2020-10-26 21:05:00 -0600

    servicios de factores

--- front-end-app/src/app/models/factores.ts
+++ front-end-app/src/app/models/factores.ts
@@ -26,7 +26,7 @@ export interface IActualizarAgregarFactores{
     TxtToken: string;
 }
 
-export class ActualizarAgregarFactores{
+export class ActualizarAgregarFactores implements IActualizarAgregarFactores{
     IdFactor: number;
     TxtFactor: string;
     TxtDescripcion: string;
@@ -35,6 +35,54 @@ export class ActualizarAgregarFactores{
     constructor(){
         this.IdFactor = 0;
         this.TxtFactor = '';
+        this.TxtDescripcion = '';
+        this.TxtToken = '';
+    }
+}
+
+export interface IDatosSubFactores{
+    IdSubFactor: number;
+    IdFactor: number;
+    TxtSubFactor: string;
+    TxtDescripcion: string;
+    FechaIngreso: string;
+}
+
+export class DatosSubFactores implements IDatosSubFactores{
+    IdSubFactor: number;
+    IdFactor: number;
+    TxtSubFactor: string;
+    TxtDescripcion: string;
+    FechaIngreso: string;
+
+    constructor(){
+        this.IdSubFactor = 0;
+        this.IdFactor = 0;
+        this.TxtSubFactor = '';
+        this.TxtDescripcion = '';
+        this.FechaIngreso = '';
+    }
+}
+
+export interface IActualizarAgregarSubFactores{
+    IdSubFactor: number;
+    IdFactor: number;
+    TxtSubFactor: string;
+    TxtDescripcion: string;
+    TxtToken: string;
+}
+
+export class ActualizarAgregarSubFactores implements IActualizarAgregarSubFactores{
+    IdSubFactor: number;
+    IdFactor: number;
+    TxtSubFactor: string;
+    TxtDescripcion: string;
+    TxtToken: string;
+
+    constructor(){
+        this.IdSubFactor = 0;
+        this.IdFactor = 0;
+        this.TxtSubFactor = '';
         this.TxtDescripcion = '';
         this.TxtToken = '';
     }
--- front-end-app/src/app/services/sub-factores.service.ts
+++ front-end-app/src/app/services/sub-factores.service.ts
@@ -1,9 +1,55 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment.prod';
+import { ActualizarAgregarSubFactores, DatosSubFactores } from 'app/models/factores';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubFactoresService {
+  subfactor = {
+    IdSubFactor: 0,
+    TxtToken: ''
+  };
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  ServicioObtenerFactores(): Observable<any>{
+    this.subfactor.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ObtenerFactores`, this.subfactor);
+  }
+
+  ServicioObtenerDatosFactor(id: number): Observable<any>{
+    this.subfactor.IdSubFactor = id;
+    this.subfactor.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ObtenerDatosFactor`, this.subfactor);
+  }
+
+  ServerEliminarFactor(subfactor: DatosSubFactores): Observable<any>{
+    this.subfactor.IdSubFactor = subfactor.IdSubFactor;
+    this.subfactor.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}EliminarFactor`, this.subfactor);
+  }
+
+  ServerAgregarFactor(subfactor: ActualizarAgregarSubFactores): Observable<any>{
+    subfactor.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}AgregarFactor`, subfactor);
+  }
+
+  ServerActualizarFactor(subfactor: ActualizarAgregarSubFactores): Observable<any>{
+    subfactor.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ActualizarFactor`, subfactor);
+  }
+
+  getToken(){
+    return JSON.parse(sessionStorage.getItem("DatosUsuario"));
+  }
 }
