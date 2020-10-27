f985c4bb2cea5a2267b9b8750a9ef6c4b7f39369
PsMorales <luispa.sosa@gmail.com>
2020-10-27 12:17:32 -0600

    actualizar escala de calificacion

--- front-end-app/src/app/services/escalas-de-calificacion.service.ts
+++ front-end-app/src/app/services/escalas-de-calificacion.service.ts
@@ -42,6 +42,12 @@ export class EscalasDeCalificacionService {
     return this.httpClient.post(`${environment.AUTH_SERVER}AgregarEscalasDeCalificacion`, escalaDeCalificacion);
   }
 
+  ServerActualizarEscalaDeCalificacion(escalaDeCalificacion: any): Observable<any>{
+    escalaDeCalificacion.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ActualizarEscalasDeCalificacion`, escalaDeCalificacion);
+  }
+
   getToken(){
     return JSON.parse(sessionStorage.getItem("DatosUsuario"));
   }
