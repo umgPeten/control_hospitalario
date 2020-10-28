f7932b7fd5fbf2524c30aacd3c5dd8b45c119efb
PsMorales <luispa.sosa@gmail.com>
2020-10-28 14:31:30 -0600

    servicios de tipos de evaluacion

--- front-end-app/src/app/services/tipos-de-evaluaciones.service.ts
+++ front-end-app/src/app/services/tipos-de-evaluaciones.service.ts
@@ -1,9 +1,55 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment.prod';
+import { ActualizarAgregarTipoDeEvaluacion, DatosTipoDeEvaluacion } from 'app/models/tipos-de-evaluaciones';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TiposDeEvaluacionesService {
+  tipoDeEvaluacion = {
+    IdTipoDeEvaluacion: 0,
+    TxtToken: ''
+  };
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  ServicioObtenerTiposDeEvaluacionesService(): Observable<any>{
+    this.tipoDeEvaluacion.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ObtenerTiposDeEvaluacion`, this.tipoDeEvaluacion);
+  }
+
+  ServicioObtenerDatosTipoDeEvaluacion(id: number): Observable<any>{
+    this.tipoDeEvaluacion.IdTipoDeEvaluacion = id;
+    this.tipoDeEvaluacion.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ObtenerDatosTipoDeEvaluacion`, this.tipoDeEvaluacion);
+  }
+
+  ServerEliminarTipoDeEvaluacion(tipoDeEvaluacion: DatosTipoDeEvaluacion): Observable<any>{
+    this.tipoDeEvaluacion.IdTipoDeEvaluacion = tipoDeEvaluacion.IdTipoDeEvaluacion;
+    this.tipoDeEvaluacion.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}EliminarTipoDeEvaluacion`, this.tipoDeEvaluacion);
+  }
+
+  ServerAgregarTipoDeEvaluacion(tipoDeEvaluacion: ActualizarAgregarTipoDeEvaluacion): Observable<any>{
+    tipoDeEvaluacion.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}AgregarTiposDeEvaluacion`, tipoDeEvaluacion);
+  }
+
+  ServerActualizarTipoDeEvaluacion(tipoDeEvaluacion: ActualizarAgregarTipoDeEvaluacion): Observable<any>{
+    tipoDeEvaluacion.TxtToken = this.getToken().TxtToken;
+
+    return this.httpClient.post(`${environment.AUTH_SERVER}ActualizarTipoDeEvaluacion`, tipoDeEvaluacion);
+  }
+
+  getToken(){
+    return JSON.parse(sessionStorage.getItem("DatosUsuario"));
+  }
 }
