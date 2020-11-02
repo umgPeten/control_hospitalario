27694c2bceabd9d73697f71781bf05e9d7987293
PsMorales <luispa.sosa@gmail.com>
2020-11-02 11:25:15 -0600

    permisos de tipos de evaluacion

--- front-end-app/src/app/components/menu/tipos-de-evaluaciones/tipos-de-evaluaciones.component.ts
+++ front-end-app/src/app/components/menu/tipos-de-evaluaciones/tipos-de-evaluaciones.component.ts
@@ -1,3 +1,4 @@
+import { Menu } from './../../../models/usuarios';
 import { DialogoConfirmacionComponent } from 'app/components/emergentes/dialogo-confirmacion/dialogo-confirmacion.component';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
@@ -10,6 +11,7 @@ import { MatSort } from '@angular/material/sort';
 import { TiposDeEvaluacionesService } from 'app/services/tipos-de-evaluaciones.service';
 import Swal from 'sweetalert2';
 import { DialogoTiposDeEvaluacionComponent } from 'app/components/emergentes/menu/dialogo-tipos-de-evaluacion/dialogo-tipos-de-evaluacion.component';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-tipos-de-evaluaciones',
@@ -23,16 +25,36 @@ export class TiposDeEvaluacionesComponent implements OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
+  menu: Menu[];
+  location: Location;
+  permisos: Menu;
+
   constructor(
     private tiposDeEvaluacionesService: TiposDeEvaluacionesService,
     private spinner: NgxSpinnerService,
     public dialogo: MatDialog,
     private router: Router,
-  ) { }
+    location: Location,
+  ) { 
+    this.location = location;
+  }
 
   ngOnInit(): void {
+    this.verificarPermisos();
     this.paginator._intl.itemsPerPageLabel = 'Elementos por pagina';
     this.cargarTiposDeEvaluacion();
+  }
+
+  verificarPermisos(){
+    var path = this.location.prepareExternalUrl(this.location.path());
+    this.menu = JSON.parse(sessionStorage.getItem("Menu"));
+
+    for(let item of this.menu){
+      item.TxtLink = '/'+item.TxtLink;
+      if(item.TxtLink === path){
+        this.permisos = item;
+      }
+    }
   }
 
   cargarTiposDeEvaluacion(){
