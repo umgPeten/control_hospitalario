3bc1c4c81abb77fd52ea18ff23bd518711d4620b
PsMorales <luispa.sosa@gmail.com>
2020-11-07 00:51:40 -0600

    evaluaciones aplicada detalle arreglada

--- front-end-app/src/environments/environment.prod.ts
+++ front-end-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // AUTH_SERVER: "https://api-sosa.cetcom.edu.gt/api/"
-  AUTH_SERVER: "http://localhost:51366/api/"
+  AUTH_SERVER: "https://api-sosa.cetcom.edu.gt/api/"
+  // AUTH_SERVER: "http://localhost:51366/api/"
 };
