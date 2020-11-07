26fd8ae955ba5d6a4149681ca021ded56e363cd1
DanexGT <alexjr64@hotmail.com>
2020-11-06 23:25:05 -0600

    Acceso denegado a factores

--- front-end-app/src/app/components/menu/factores/factores.component.ts
+++ front-end-app/src/app/components/menu/factores/factores.component.ts
@@ -66,7 +66,7 @@ export class FactoresComponent implements OnInit {
       
     }
     else{
-      this.router.navigate(['/usuarios']);
+      this.router.navigate(['/acceso-denegado']);
     }
   }
 
